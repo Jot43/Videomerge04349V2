b8e1a2b803bdfe0713e54f86283b8a29f2b88674
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-02-10 03:34:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = True
     MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
-    UPSTREAM_REPO = "https://github.com/Darkksoul/Videomerge"
+    UPSTREAM_REPO = "https://github.com/Jot43/Videomerge04349/"
     UPSTREAM_BRANCH = "main"
 
     START_TEXT = """
