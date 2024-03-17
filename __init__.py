9507202d3d23db1b9d464ba36d6c5db6ef7785a0
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-03-17 16:51:33 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,8 +11,8 @@ MERGE_MODE = {}  # Maintain each user merge_mode
 UPLOAD_AS_DOC = {}  # Maintain each user ul_type
 UPLOAD_TO_DRIVE = {}  # Maintain each user drive_choice
 
-FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "🤪")
-UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "⬛")
+FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "🤍🥰")
+UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "🖤")
 EDIT_SLEEP_TIME_OUT = 8
 gDict = defaultdict(lambda: [])
 queueDB = {}
